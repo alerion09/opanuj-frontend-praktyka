7287e693cfa97f78873156c5c18e000e205a97cb
Kajetan Kuźma <kajetankuzma@gmail.com>
2025-04-01 21:20:31 +0200

    feat: resolve lesson4/axios-interceptor (fetch try)

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 let startTime = 0;
+
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
   startTime = new Date().getTime();
@@ -18,8 +19,32 @@ axios.interceptors.response.use(function (response) {
   return response;
 });
 
-const {
-  data: articles,
-} = await axios.get('/api/data/articles?timeout=3000');
+const { data: articles } = await axios.get('/api/data/articles?timeout=3000');
+
+const globalFetch = new Proxy(fetch, {
+  apply: async (target, thisArg, argumentsList) => {
+    const [url] = argumentsList;
+    const startTimeFetch = performance.now();
+
+    try {
+      const response = await target.apply(thisArg, argumentsList);
+      const requestTime = performance.now() - startTimeFetch;
+      console.log(`Request to ${url} took ${requestTime}ms`);
+
+      return response;
+    } catch (e) {
+      const requestTime = performance.now() - startTimeFetch;
+      console.log(`Request to ${url} took ${requestTime}ms`);
+
+      throw e;
+    }
+  },
+});
+
+const articlesFetchResult = await globalFetch(
+  '/api/data/articles?timeout=3000'
+);
+const articlesFetch = await articlesFetchResult.json();
 
 document.querySelector('#data').innerHTML = articles?.[0]?.content;
+document.querySelector('#data-fetch').innerHTML = articlesFetch?.[0]?.content;
