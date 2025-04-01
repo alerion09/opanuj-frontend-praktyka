1968193794a3ee79baecd2bfe0aae84e04ce65e7
Kajetan Kuźma <kajetankuzma@gmail.com>
2025-04-01 20:26:19 +0200

    feat: resolve lesson4/axios-interceptor (axios)

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -1,18 +1,25 @@
 import axios from 'axios';
 
+let startTime = 0;
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  startTime = new Date().getTime();
+
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
   // Do something with response data
+  const requestTime = new Date().getTime() - startTime;
+  console.log(`Request to ${response.config.url} took ${requestTime}ms`);
+  startTime = 0;
+
   return response;
 });
 
 const {
-  data: { articles },
+  data: articles,
 } = await axios.get('/api/data/articles?timeout=3000');
 
-document.querySelector('#data').innerHTML = articles[0].content;
+document.querySelector('#data').innerHTML = articles?.[0]?.content;
